7db5607e9b428ce0f214364ea062166ce4c993bf
kimyeonjin <kyj9750322@gmail.com>
2024-02-19 21:56:01 +0900

    메서드 수정

--- src/main/java/com/example/solchall2024/controller/GCScontroller.java
+++ src/main/java/com/example/solchall2024/controller/GCScontroller.java
@@ -1,23 +1,35 @@
 package com.example.solchall2024.controller;
 
-import com.example.solchall2024.request.GCSrequest;
+import com.example.solchall2024.dto.DownloadReqDto;
+import com.example.solchall2024.dto.UploadReqDto;
 import com.example.solchall2024.service.GCSservice;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+
 @RestController
 @RequiredArgsConstructor
 public class GCScontroller {
-    private final GCSservice solchallservice;
 
-    @PostMapping("/api/solchall/upload")
-    public ResponseEntity<Void> objectUpload(GCSrequest solchallrequest) throws IOException{
-        solchallservice.uploadObject(solchallrequest);
-        return new ResponseEntity<>(HttpStatus.OK);
+    private final GCSservice gcsservice;
+
+    @PostMapping("gcs/download")
+    public ResponseEntity localDownloadFromStorage(@RequestBody DownloadReqDto downloadReqDto) throws IOException {
+        String localFileLocation = "C:\\github\\solutionchallenge";
+        Blob fileFromGCS = gcsservice.downloadFileFromGCS(downloadReqDto, localFileLocation);
+        return ResponseEntity.ok(fileFromGCS.toString());
     }
+    @PostMapping("gcs/upload")
+    public <uploadReqDto, UploadDto> ResponseEntity localUploadToStorage(@RequestBody UploadReqDto uploadReqDto) throws IOException {
+        BlobInfo fileFromGCS = gcsservice.uploadFileToGCS(uploadReqDto);
+        return ResponseEntity.ok(fileFromGCS.toString());
+    }
+
 }
--- src/main/java/com/example/solchall2024/service/GCSservice.java
+++ src/main/java/com/example/solchall2024/service/GCSservice.java
@@ -1,35 +1,51 @@
 package com.example.solchall2024.service;
 
-import com.example.solchall2024.request.GCSrequest;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-
+import com.example.solchall2024.dto.DownloadReqDto;
+import com.example.solchall2024.dto.UploadReqDto;
+import com.google.cloud.storage.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Paths;
 
 @Service
+@RequiredArgsConstructor
 public class GCSservice {
 
-    private String bucketName;
+    private final Storage storage;
 
-    public void uploadObject(GCSrequest solchallrequest)throws IOException{
+    public BlobInfo uploadFileToGCS(UploadReqDto uploadReqDto) throws IOException {
+        String localFileLocation = uploadReqDto.getLocalFileLocation();
+        String uploadFileName = uploadReqDto.getUploadFileName();
+        String bucketName = uploadReqDto.getBucketName();
+        String contentType = uploadReqDto.getContentType();
 
-        String keyFileName = "omega-moonlight-413423-61d3b94a83d8.json";
-        InputStream keyFile = ResourceUtils.getURL("classpath:" + keyFileName).openStream();
-        
-        Storage storage = StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(keyFile))
-                .build()
-                .getService();
-
-        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName,solchallrequest.getFile().getOriginalFilename())
-                .setContentType(solchallrequest.getFile().getContentType()).build();
-        
-        BlobInfo blob = ((Storage) storage).create(blobInfo, solchallrequest.getFile().getInputStream());
+        try {
+            BlobInfo blobInfo = storage.create(
+                    BlobInfo.newBuilder(bucketName, uploadFileName)
+                            .setContentType(contentType)
+                            .build(),
+                    new FileInputStream(localFileLocation));
+            return blobInfo;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e; // 업로드 실패 시 IOException 예외 전파
+        }
     }
+
+    public Blob downloadFileFromGCS(DownloadReqDto downloadReqDto, String localFileLocation) throws IOException {
+        String bucketName = downloadReqDto.getBucketName();
+        String downloadFileName = downloadReqDto.getDownloadFileName();
+
+        Blob blob = storage.get(bucketName, downloadFileName);
+        if (blob == null) {
+            throw new FileNotFoundException("Blob not found: " + downloadFileName);
+        }
+
+        blob.downloadTo(Paths.get(localFileLocation));
+        return blob;
+    }
+
 }
